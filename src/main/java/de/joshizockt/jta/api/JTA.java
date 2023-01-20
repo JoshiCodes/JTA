52e65f1736fb60fcdf9af6f3eb1cdb54ac5f5a98
JoshiCodes <joshua.bickei@icloud.com>
2023-01-20 22:30:52 +0100

    Added documentation
    This commit only adds some sort of documentation for the basic methods in the JTA and RestAction class.

--- src/main/java/de/joshizockt/jta/api/JTA.java
+++ src/main/java/de/joshizockt/jta/api/JTA.java
@@ -1,5 +1,6 @@
 package de.joshizockt.jta.api;
 
+import de.joshizockt.jta.api.exception.IllegalChatTypeException;
 import de.joshizockt.jta.api.object.User;
 import de.joshizockt.jta.api.object.chat.GenericChat;
 import de.joshizockt.jta.api.object.chat.PrivateChat;
@@ -21,14 +22,53 @@ public abstract class JTA {
     }
 
 
+    /**
+     * Get the BotUser
+     * This returns a normal User Instance
+     * @return the User Instance of the Bot
+     */
     public RestAction<User> getSelfUser() {
         return new RestAction<>((v) -> requestHandler.execute(new GetSelfRequest(this)));
     }
 
+    /**
+     * Get a Chat by ID
+     * Returns a Chat Instance with any type as GenericChat
+     * @param id the ID of the Chat
+     * @return the Chat Instance
+     */
+    public RestAction<GenericChat> getChat(long id) {
+        return getChat(String.valueOf(id));
+    }
+
+    /**
+     * Get a Chat by ID
+     * Returns a Chat Instance with any type as GenericChat
+     * @param id the ID of the Chat
+     * @return the Chat Instance
+     */
     public RestAction<GenericChat> getChat(String id) {
         return new RestAction<>((v) -> requestHandler.execute(new GetChatRequest(this, id)));
     }
 
+    /**
+     * Get a PrivateChat by ID
+     * Returns a Chat Instance with the type PrivateChat
+     * @param id the ID of the Chat
+     * @return the PrivateChat Instance
+     * @throws IllegalChatTypeException if the Chat is not a PrivateChat
+     */
+    public RestAction<PrivateChat> getPrivateChat(long id) {
+        return getPrivateChat(String.valueOf(id));
+    }
+
+    /**
+     * Get a PrivateChat by ID
+     * Returns a Chat Instance with the type PrivateChat
+     * @param id the ID of the Chat
+     * @return the PrivateChat Instance
+     * @throws IllegalChatTypeException if the Chat is not a PrivateChat
+     */
     public RestAction<PrivateChat> getPrivateChat(String id) {
         return new RestAction<>((v) -> requestHandler.execute(new GetChatRequest(this, id)).getAsPrivateChat());
     }
--- src/main/java/de/joshizockt/jta/api/rest/RestAction.java
+++ src/main/java/de/joshizockt/jta/api/rest/RestAction.java
@@ -1,5 +1,7 @@
 package de.joshizockt.jta.api.rest;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.function.BiConsumer;
@@ -10,25 +12,20 @@ import java.util.function.Predicate;
 public class RestAction<T> {
 
     private Function<Void, T> action;
-    private Consumer<Exception> error;
 
     public RestAction(Function<Void, T> action) {
         this.action = action;
     }
 
-    public RestAction<T> error(Consumer<Exception> error) {
-        this.error = error;
-        return this;
-    }
-
+    /**
+     * Executes the action and returns the result
+     * @return The result T of the action
+     */
     public T complete() {
         try {
             return action.apply(null);
         } catch (Exception e) {
-            if(error != null) {
-                error.accept(e);
-            } else throw new RuntimeException(e);
-            return null;
+            throw new RuntimeException(e);
         }
     }
 
@@ -36,11 +33,38 @@ public class RestAction<T> {
         queue(null);
     }
 
+    /**
+     * Executes the action and calls the consumer with the result
+     * @param then the consumer to call with the result, can be null
+     */
     public void queue(Consumer<T> then) {
         T t = complete();
         if(then != null) {
             then.accept(t);
         }
+    }
+
+    /**
+     * Executes the action and returns nothing.
+     * If finished successfully, the then consumer will be executed. If an error occurs, the error consumer will be executed.
+     * @param then The consumer to execute if the action was successful, can be null
+     * @param error The consumer to execute if an error occurs, can be null
+     */
+    public void queue(@Nullable Consumer<T> then, @Nullable Consumer<Exception> error) {
+        try {
+            T t = complete();
+            if(then != null) {
+                then.accept(t);
+            }
+        } catch (Exception e) {
+            if(error != null) {
+                error.accept(e);
+            } else throw new RuntimeException(e);
+        }
+    }
+
+    public CompletableFuture<T> submit() {
+        return CompletableFuture.supplyAsync(this::complete);
     }
 
 }
